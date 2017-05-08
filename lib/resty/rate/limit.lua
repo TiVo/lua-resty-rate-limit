881951f841ce82d04892b920398e026ec19b4d5b
Travis Bell <travis@fanhattan.com>
2017-05-08 09:09:13 -0600

    Adding Access-Control-Expose-Headers for the Retry-After header.

--- lib/resty/rate/limit.lua
+++ lib/resty/rate/limit.lua
@@ -103,6 +103,7 @@ function _M.limit(config)
                 retry_after = 0
             end
 
+            ngx.header["Access-Control-Expose-Headers"] = "Retry-After"
             ngx.header["Access-Control-Allow-Origin"] = "*"
             ngx.header["Content-Type"] = "application/json; charset=utf-8"
             ngx.header["Retry-After"] = retry_after
